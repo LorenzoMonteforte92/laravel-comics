c8b2e4518f6ea834b98bccd61b314119b8011a11
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-05-20 16:02:46 +0200

    first commit

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,20 @@
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-3 py-3">
+                <img src="{{ Vite::asset('resources/img/dc-logo.png') }}" alt="DC comics logo">
+            </div>
+            <div class="col-9 d-flex justify-content-center align-items-end">
+                <nav>
+                    <ul class="m-0 d-flex justify-content-center align-items-center gap-4 pt-2 " >
+                        @foreach ($navLinks as $link)
+                            <li class="active-element ms-padding">
+                                <a href="#">{{ $link }}</a>
+                            </li>
+                        @endforeach
+                    </ul>
+                </nav>
+            </div>
+        </div>
+    </div>
+</header>
