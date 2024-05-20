4f2a7b2978c0223dcd9c7eb17669c4d9e2b2484d
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-05-20 17:08:03 +0200

    feat: add complete card section

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -8,10 +8,10 @@
             <div class="row justify-content-center">
                 <div class="col-12 d-flex flex-wrap gap-3">
                     @foreach ($comics as $comic)
-                        <div class="card" style="width: calc(100% / 6 - 1rem);">
-                            <div class="ms-card-img" ><img src="..." class="card-img-top" alt="..."></div>
-                            <div class="card-body px-0">
-                                <div><p class="card-text text-white"></p></div>
+                        <div class="card ms-card" style="width: calc(100% / 6 - 1rem);">
+                            <div class="ms-card-img" ><img src="{{$comic['thumb']}}" class="card-img-top" alt="..."></div>
+                            <div class="card-body px-0  d-flex flex-column justify-content-between">
+                                <div><p class="card-text text-white">{{strtoupper($comic['series'])}}</p></div>
                             </div>
                         </div>
                     @endforeach
