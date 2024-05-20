d698138dc08b3b76c80409ca76aa63a5fe023ec9
LorenzoMonteforte92 <l.monteforte@yahoo-it>
2024-05-20 16:57:47 +0200

    feat: add hero cards

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,5 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
- 
+    <div class="hero" ></div>
+    <section class="position-relative">
+        <div class="current-series position-absolute py-2 px-4"><h4 class="fw-bold text-white m-0" >CURRENT SERIES</h4></div>
+        <div class="container py-5 d-flex flex-column">
+            <div class="row justify-content-center">
+                <div class="col-12 d-flex flex-wrap gap-3">
+                    @foreach ($comics as $comic)
+                        <div class="card" style="width: calc(100% / 6 - 1rem);">
+                            <div class="ms-card-img" ><img src="..." class="card-img-top" alt="..."></div>
+                            <div class="card-body px-0">
+                                <div><p class="card-text text-white"></p></div>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+            <button class="m-auto px-5 py-1 my-3">LOAD MORE</button>
+        </div>
+    </section>
 @endsection
